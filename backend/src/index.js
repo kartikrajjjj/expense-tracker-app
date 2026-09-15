ee486caf3670b1e208fa06cf2328debce124552c
kartikrajjjj <kartikraj9756@gmail.com>
2026-09-16 01:36:43 +0530

    Add user authentication with login and JWT cookies
    Implement login controller with user lookup and bcrypt password verification. Generate JWT authentication tokens and store them in HTTP-only cookies. Add cookie-parser and authentication-related dependencies.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import morgan from "morgan";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 dotenv.config();
 
 import userRouter from "./user/user.routes.js";
 
 const app = express();
+app.use(cookieParser());
 
 app.use(express.json());
 
@@ -17,6 +20,9 @@ app.use("/api/user", userRouter);
 
 app.listen(5000, () => {
     console.log("Server running on port 5000");
+});
+app.get("/",(req,res)=>{
+    res.send("Welcome to expense Tracker API");
 });
 import mongoose from "mongoose";
 mongoose.connect(process.env.DB_URL)
--- backend/src/user/user.controller.js
+++ backend/src/user/user.controller.js
@@ -1,4 +1,6 @@
 import UserModel from "./user.model.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const createUser = async (req,res)=>{
     try{
@@ -11,10 +13,37 @@ export const createUser = async (req,res)=>{
     }
 }
 
+const createToken = async (user)=>{
+    const  payload ={
+        id: user._id,
+        fullname: user.fullname,
+        email: user.email,
+        role: user.role
+    } 
+    const token = await jwt.sign(payload, process.env.AUTH_SECRET,{expiresIn: "1d"});
+    return token;
+}
+
 export const login = async (req,res)=>{
     try{
-        const data = req.body;
-        console.log(data);
+        const {email, password} = req.body;
+        const user = await UserModel.findOne({email});
+        if(!user){
+            return res.status(404).json({message: "User does not exist"});
+        }
+        const isLogged = await bcrypt.compare(password,user.password);
+        if(!isLogged){
+            return res.status(401).json({message: "Incorrect Password"});
+        } 
+        const token =await  createToken(user);
+        res.cookie("authToken", token,{
+            maxAge : 86400000,
+            domain : process.env.ENVIRONMENT ==="DEV"? "localhost" : process.env.DOMAIN,
+            secure : process.env.ENVIRONMENT ==="DEV"? false : true,
+            httpOnly : true
+        });
+        res.json({message: "Login successful"});
+
     } catch(err){
         res.status(500).json({message : err.message});
     }
