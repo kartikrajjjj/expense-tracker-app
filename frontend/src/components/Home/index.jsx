e3316ac9da01b159da8cb6e99e1f07537afc6533
kartikrajjjj <kartikraj9756@gmail.com>
2026-09-16 02:21:38 +0530

    Set up frontend routing and reusable layout

---
+++ frontend/src/components/Home/index.jsx
@@ -0,0 +1,12 @@
+import Homelayout from "../../layout/Homelayout";
+
+const HomePage = ()=>{
+    return (
+        <Homelayout>
+            <h1 className="text-5xl text-green-500">
+                Login Page
+            </h1>
+        </Homelayout>
+    )
+}
+export default HomePage;
